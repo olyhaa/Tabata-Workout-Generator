8c6dbe78657e751aa74d9209fc9a630caa9298f0
Amanda Olyha <amanda.olyha@gmail.com>
2022-11-21 14:34:40 -0500

    cleanup

--- src/app/exercise-list/exercise-list.component.ts
+++ src/app/exercise-list/exercise-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { MatSort, Sort } from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../types';
 import { SelectionModel } from '@angular/cdk/collections';
