dfca157d3263512db7830f2d7f50ab6a94acdbf3
Amanda Olyha <amanda.olyha@gmail.com>
2023-01-11 12:40:09 -0500

    use form group

--- src/app/workout-params/workout-params.component.ts
+++ src/app/workout-params/workout-params.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Exercise, WorkoutParams } from '../types';
 import { SelectionModel } from '@angular/cdk/collections';
 import { DEFAULT_NUM_CYCLES, DEFAULT_NUM_SETS, DEFAULT_PREPARE_TIME, DEFAULT_REST_TIME, DEFAULT_WORK_TIME } from '../constants';
@@ -14,13 +14,14 @@ export class WorkoutParamsComponent implements OnInit {
   @Output() newParamsEvent = new EventEmitter<WorkoutParams>;
   exerciseList: Exercise[] = [];
 
-  title = new FormControl("");
-  numSets = new FormControl(DEFAULT_NUM_SETS);
-  numCycles = new FormControl(DEFAULT_NUM_CYCLES);
-
-  prepareTime = new FormControl(DEFAULT_PREPARE_TIME);
-  workTime = new FormControl(DEFAULT_WORK_TIME);
-  restTime = new FormControl(DEFAULT_REST_TIME);
+  workoutForm = new FormGroup({
+    title: new FormControl(""),
+    numSets: new FormControl(DEFAULT_NUM_SETS),
+    numCycles: new FormControl(DEFAULT_NUM_CYCLES),
+    prepareTime: new FormControl(DEFAULT_PREPARE_TIME),
+    workTime: new FormControl(DEFAULT_WORK_TIME),
+    restTime: new FormControl(DEFAULT_REST_TIME)
+  });
 
   initialSelection = [];
   selection = new SelectionModel<Exercise>(true, this.initialSelection);
@@ -33,12 +34,12 @@ export class WorkoutParamsComponent implements OnInit {
 
   getParams() {
     const newParams: WorkoutParams = {
-      title: this.title.value || "",
-      numSets: this.numSets.value || DEFAULT_NUM_SETS,
-      numCycles: this.numCycles.value || DEFAULT_NUM_CYCLES,
-      prepareTime: this.prepareTime.value || DEFAULT_PREPARE_TIME,
-      workTime: this.workTime.value || DEFAULT_WORK_TIME,
-      restTime: this.restTime.value || DEFAULT_REST_TIME,
+      title: this.workoutForm.value.title || "",
+      numSets: this.workoutForm.value.numSets || DEFAULT_NUM_SETS,
+      numCycles: this.workoutForm.value.numCycles || DEFAULT_NUM_CYCLES,
+      prepareTime: this.workoutForm.value.prepareTime || DEFAULT_PREPARE_TIME,
+      workTime: this.workoutForm.value.workTime || DEFAULT_WORK_TIME,
+      restTime: this.workoutForm.value.restTime || DEFAULT_REST_TIME,
       exerciseList: this.selection.selected
     }
     this.newParamsEvent.emit(newParams);
